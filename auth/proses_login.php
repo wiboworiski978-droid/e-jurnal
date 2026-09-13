90c6f172676f0b48cf73ee44c3f4706cc9897a92
Riski <wiboworiski978@gmail.com>
2026-09-13 11:47:20 +0700

    membuat proses login

--- auth/proses_login.php
+++ auth/proses_login.php
@@ -2,8 +2,98 @@
 
 session_start();
 
-require_once "../config/koneksi.php";
+require_once __DIR__ . "/../config/koneksi.php";
 
 $role = $_POST['role'] ?? '';
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
+
+if (empty($role) || empty($username) || empty($password)) {
+    die("semua data wajib diisi.");
+}
+
+// login siswa
+if ($role === "siswa") {
+
+    $query = "SELECT * FROM siswa WHERE nisn = ?";
+
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+    $siswa = mysqli_fetch_assoc($result);
+
+    if ($siswa && password_verify($password, $siswa['password'])) {
+
+        $_SESSION['login'] = true;
+        $_SESSION['role'] = "siswa";
+        $_SESSION['id_siswa'] = $siswa['id_siswa'];
+        $_SESSION['nama'] = $siswa['nama_lengkap'];
+
+        header("Location: ../siswa/dashboard.php");
+        exit;
+    } else {
+        
+        header("Location: login.php?error=Login siswa gagal");
+        exit;
+    }
+}
+
+// login guru
+
+if ($role === "guru") {
+    $query = "SELECT * FROM guru WHERE nip = ?";
+
+    $stmt =mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+    $guru = mysqli_fetch_assoc($result);
+
+    if ($guru && password_verify($password, $guru['password'])) {
+
+        $_SESSION['login'] = true;
+        $_SESSION['role'] = "guru";
+        $_SESSION['id_guru'] = $guru['id_guru'];
+        $_SESSION['nama'] = $guru['nama_guru'];
+
+        header("Location: ../guru/dashboard.php");
+        exit;
+
+    } else {
+        header("Location: login.php?error=Login guru gagal");
+        exit;
+    }
+}
+
+
+// login admin
+if ($role === "admin") {
+    $query = "SELECT * FROM admin WHERE username = ?";
+
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+    $admin = mysqli_fetch_assoc($result);
+
+    if ($admin && password_verify($password, $admin['kode_unik'])) {
+        
+        $_SESSION['login'] = true;
+        $_SESSION['role'] = "admin";
+        $_SESSION['id_admin'] = $admin['id_admin'];
+        $_SESSION['nama'] = $admin['username'];
+
+        header("Location: ../admin/dahboard.php");
+    } else {
+        header("Location: login.php?error=Login admin gagal");
+        exit;
+    }
+}
+
+// role tidak valid
+header("Location: login.php?error=Role tidak valid");
+exit;
---
+++ buat_hash.php
@@ -0,0 +1,3 @@
+<?php
+
+echo password_hash("ADMIN123", PASSWORD_DEFAULT);
